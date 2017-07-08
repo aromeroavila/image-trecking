ff0ae01991c1acf52c910d8391499ffe5d4b60c3
Angel Romero <aromeroavila@gmail.com>
2017-07-08 10:58:31 +0100

    Empty Images Activity

---
+++ app/src/main/java/com/arao/imagetrecking/presentation/ImagesActivity.java
@@ -0,0 +1,15 @@
+package com.arao.imagetrecking.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.arao.imagetrecking.R;
+
+public class ImagesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_images);
+    }
+}
